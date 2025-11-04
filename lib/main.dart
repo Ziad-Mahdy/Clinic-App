a8fdf29b190d0bcc7502cbdb7342ce36540883cd
Mahdy <zyadmahdy01@gmail.com>
2025-11-04 14:22:51 +0200

    third

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Clinic Ap",
+      title: "Clinic App",
       home: RoleSelectionPage(),
     );
   }
